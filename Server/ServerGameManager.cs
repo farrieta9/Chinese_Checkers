387576e035865b0580ea1b5104ca7a345cd80ca8
HGunther <hansgunther@aggiemail.usu.edu>
2015-11-17 15:56:40 -0700

    Outlined GameManager and ServerGameManager

---
+++ Server/ServerGameManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class ServerGameManager : CheckersLib.GameManager
+    {
+        ServerGameManager()
+        {
+            
+        }
+
+        ~ServerGameManager()
+        {
+
+        }
+
+        private void ValidateMove()
+        {
+
+        }
+
+        private void MakeNPCMove()
+        {
+
+        }
+    }
+}
