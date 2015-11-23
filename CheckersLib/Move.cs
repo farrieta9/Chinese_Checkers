a883eae09c22b9089a5241d949a0d3ee4af4c291
HGunther <hansgunther@aggiemail.usu.edu>
2015-11-23 13:27:25 -0700

    Added Move class type

---
+++ CheckersLib/Move.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckersLib
+{
+    class Move
+    {
+        public Space Player;
+        public int startX;
+        public int startY;
+        public int endX;
+        public int endY;
+    }
+}
