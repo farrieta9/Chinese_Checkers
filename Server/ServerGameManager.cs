676fffe08eb96ba93be9d228abf0b380ae93fa0e
HGunther <hansgunther@aggiemail.usu.edu>
2015-11-23 13:19:33 -0700

    Psudocoded ServerGameManager class

--- Server/ServerGameManager.cs
+++ Server/ServerGameManager.cs
@@ -20,12 +20,22 @@ namespace Server
 
         private void ValidateMove()
         {
+            //Recieve as input a players move
+            //If it is legal for that piece to move to that location
+            //return true
+            //else
+            //return false
+        }
 
+        private void updatePlayerMove()
+        {
+            
         }
 
         private void MakeNPCMove()
         {
-
+            //scan through all the players pieces calling list legal moves on each one, and recording the move which will move a piece the farthest
+            //when finished with scan, make the move that has been saved
         }
     }
 }
